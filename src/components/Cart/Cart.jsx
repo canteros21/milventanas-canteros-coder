feb1848adccb9d7c820955536f0dfee3f9d711d0
canteros21 <canteros21@gmail.com>
2021-12-01 22:25:47 -0300

    Clase 13 - Firebase II
    Se insertó el funcionamiento de generar oredenes de compras que se guardan en firebase

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,8 +1,11 @@
 import { useState, useEffect, memo } from 'react'
-import { Container, Row, Col, Button } from 'react-bootstrap'
+import { Container, Row, Col, Button, Modal } from 'react-bootstrap'
 import { useCartContext } from '../../context/CartContext'
 import CartListItem from '../CartListItem/CartListItem.jsx'
 import { Link } from 'react-router-dom'
+import firebase from "firebase"
+import 'firebase/firestore'
+import { getFirestore } from '../../services/getFirestore.js'
 
 import './Cart.css';
 
@@ -11,6 +14,8 @@ const Cart = memo(() => {
     const { quitarTodo, cartList, precioTotal, borrarDelCarro } = useCartContext()
 
     const [isModified, setIsModified] = useState(false)
+    const [showModal, setShowModal] = useState(false)
+    const [orderId, setOrderId] = useState("")
 
 
     useEffect(() => {
@@ -32,6 +37,59 @@ const Cart = memo(() => {
         quitarTodo()
     }
 
+    const generarOrden = () => {
+
+        let orden = {}
+        orden.date = firebase.firestore.Timestamp.fromDate(new Date())
+        orden.total = precioTotal()
+        orden.buyer = { nombre: "Nicolas Canteros", mail: "canteros21@gmail.com", tel: "48218180" }
+        orden.items = cartList.map(cartItem => {
+            const id = cartItem.id
+            const nombre = cartItem.nombre
+            const precio = cartItem.precio * cartItem.cantidadAgregada
+
+            return { id, nombre, precio }
+        })
+
+
+        const dbQuery = getFirestore()
+        dbQuery.collection("ordenes").add(orden)
+            .then(resp => {
+                console.log(resp)
+                setOrderId(resp.id)
+            })
+            .catch(err => console.log(err))
+            .finally(() => {
+                setShowModal(true)
+            })
+
+        /*const itemsToUpdate = dbQuery.collection("productos").where(
+            firebase.firestore.FieldPath.documentId(), "in", cartList.map(i => i.id)
+        )
+
+        const batch = dbQuery.batch()
+
+        itemsToUpdate.get()
+            .then(collection => {
+                collection.docs.forEach(currentStock => {
+                    batch.update(currentStock.ref, {
+                        stock: currentStock.data().stock - cartList.find(item => item.id === currentStock.id).cantidadAgregada
+                    })
+                })
+
+                batch.commit().then(res => {
+                    console.log('resultado batch:', res);
+                })
+            })
+        */
+
+
+        console.log(orden)
+
+    }
+
+
+    const handleClose = () => { setShowModal(false) }
 
     return (
         <>
@@ -56,6 +114,17 @@ const Cart = memo(() => {
                                 <h3 className="total-label">Total: <b>${precioTotal()}</b></h3>
                                 <button className="btn-quitar-todo" onClick={removeAll}>Quitar todos los productos</button>
                             </Col>
+                        </Row>
+                        <Row>
+                            <Col xs={{ span: 3, offset: 9 }}>
+                                <button className="btn-terminar-compra" onClick={generarOrden}>Terminar Compra</button>
+                            </Col>
+                            <Modal show={showModal} onHide={handleClose}>
+                                <Modal.Header closeButton>
+                                    <Modal.Title>¡Gracias por su compra!</Modal.Title>
+                                </Modal.Header>
+                                <Modal.Body>El código de seguimiento de su orden es: <b>{orderId}</b></Modal.Body>
+                            </Modal>
                         </Row>
                     </Container >
                     :
